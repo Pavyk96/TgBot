79adc5e459db382c1a97cb2bded2d8c3fe8058a3
daniil <pepethefrog27@mail.ru>
2025-02-20 18:44:35 +0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.pavyk96"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// Spring Boot Starter
+	implementation("org.springframework.boot:spring-boot-starter")
+
+	// Lombok для сокращения бойлерплейт-кода
+	implementation("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	// PostgreSQL драйвер
+	implementation("org.postgresql:postgresql")
+
+	// Spring Data JPA для работы с базой
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	// Telegram Bots API
+	implementation("org.telegram:telegrambots:6.7.0")
+
+	// Тестирование
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
